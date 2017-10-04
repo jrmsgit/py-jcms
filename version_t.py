ea73624491ec338ab7a75d520b4ea339c086986d
Jeremías <jrmsdev@gmail.com>
2017-10-04 11:35:13 -0300

    version mod tests

---
+++ version_t.py
@@ -0,0 +1,79 @@
+from os import write, close, unlink
+from unittest import TestCase
+from tempfile import mkstemp
+import version
+
+mods = [
+    'jcindex.__init__',
+    'jcindex.admin',
+    'jcindex.apps',
+    'jcindex.migrations.__init__',
+    'jcindex.models',
+    'jcindex.views',
+    'jcms.__init__',
+    'jcms.response',
+    'jcms.settings',
+    'jcms.urls',
+    'jcms.wsgi',
+    'jcmsmain',
+    'version',
+]
+testmods = mods.copy ()
+testmods.extend ([
+    'jcindex.views_t',
+    'jcms.response_t',
+    'jcmstest',
+    'version_t',
+])
+testmods = sorted (testmods)
+
+class TestVersion (TestCase):
+    _version = None
+    _versionString = None
+
+    def setUpClass ():
+        TestVersion._version = '{}.{}'.format (version.VMAJOR, version.VMINOR)
+        if version.VPATCH > 0:
+            TestVersion._version = '{}.{}'.format (TestVersion._version, version.VPATCH)
+        TestVersion._versionString = '{} v{}'.format (version.APPNAME, TestVersion._version)
+
+    def testGet (t):
+        t.assertEqual (t._version, version.get ())
+
+    def testGetVPATCH (t):
+        oldVPATCH = version.VPATCH
+        try:
+            version.VPATCH = 999
+            t.assertTrue (version.get ().endswith ('.999'))
+        finally:
+            version.VPATCH = oldVPATCH
+
+    def testString (t):
+        t.assertEqual (t._versionString, version.string ())
+
+    def testCatfile (t):
+        fd, fn = mkstemp (suffix = '-jcmstestversion')
+        try:
+            write (fd, b'L1\n')
+            write (fd, b'L2\n')
+            write (fd, b'L3\n')
+            close (fd)
+        except Exception as e:
+            unlink (fn)
+            raise e
+        try:
+            t.assertListEqual (['L1', 'L2', 'L3'], version.catfile (fn).split ())
+        finally:
+            unlink (fn)
+
+    def testInstallModules (t):
+        t.assertListEqual (mods, version.installModules ())
+
+    def testInstallModulesTests (t):
+        t.assertListEqual (testmods,
+                version.installModules (tests = True))
+
+    def testInstallFiles (t):
+        t.assertListEqual ([
+            ('', ['LICENSE', 'README.rst']),
+        ], version.installFiles ())
