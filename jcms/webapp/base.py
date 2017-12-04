00c1acff9464f97c9f08a64bade8acd65beb0c5b
Jeremías <jrmsdev@gmail.com>
2017-12-04 03:51:00 -0300

    run webapp

--- jcms/webapp/base.py
+++ jcms/webapp/base.py
@@ -1,7 +1,30 @@
+from os import path, environ
 from jcms.cmd import maincmd
 
 class Webapp (object):
-    name = None
+    __fn = None
+    __srcdir = None
+    __name = None
+    # ~ __cfg = None
+
+    def __init__ (self, filename):
+        self.__checkFilename (filename)
+        self.__fn = path.realpath (filename)
+        self.__srcdir = path.dirname (self.__fn)
+        self.__name = path.basename (self.__srcdir)
+        # ~ self.__loadCfg ()
+
+    def __checkFilename (self, filename):
+        if not path.isfile (filename):
+            raise RuntimeError ('{}: jcms webapp filename not found!')
+
+    # ~ def __loadCfg (self):
+        # ~ cf = path.join (self.__srcdir, 'jcms.cfg')
+        # ~ if not path.isfile (cf):
+            # ~ raise RuntimeError ('{}: config file not found!'.format (cf))
+        # ~ # FIXME: self.__cfg = ...
 
     def run (self):
+        environ.setdefault ('DJANGO_SETTINGS_MODULE', '{}.settings'.format (self.__name))
+        print ('webapp:', self.__name)
         maincmd.run ()
