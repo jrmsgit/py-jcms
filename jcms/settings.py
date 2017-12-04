89c8f4a0c83107ff53db88dddd4dfefbfe6679b5
Jeremías <jrmsdev@gmail.com>
2017-12-03 21:46:04 -0300

    relocate lang files

--- jcms/settings.py
+++ jcms/settings.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname (os.path.dirname (os.path.abspath (__file__)))
+BASE_DIR = os.path.dirname (os.path.dirname (os.path.realpath (__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -134,4 +134,4 @@ LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/user/'
 LOGOUT_REDIRECT_URL = '/auth/login/'
 
-LOCALE_PATHS = [os.path.join (BASE_DIR, 'jcmslang')]
+LOCALE_PATHS = [os.path.join (BASE_DIR, 'jcms', 'lang')]
